972c027806f633f0ed01d324fe7d1c196d2e183f
Dharanish.G <157550133+vasudhevdharanish@users.noreply.github.com>
2024-01-24 08:13:06 +0530

    Exercise.java

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.StringTokenizer;
-
+//Problem 2
 public class Exercise {
         public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
