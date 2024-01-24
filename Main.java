e8d37c79c827afb62a2c8dfcb86b465ace12889b
Dharanish.G <157550133+vasudhevdharanish@users.noreply.github.com>
2024-01-24 08:15:31 +0530

    Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.io.Console;
 import java.io.*;;
 import java.util.StringTokenizer;
 //problem 1
-public class Exercise1{
+public class Main{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the sentence:");
